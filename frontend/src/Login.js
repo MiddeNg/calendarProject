4ec59f6b8c39bc4d191682f5f534b39285f3336d
William <williamng@ubizense.com>
2024-12-03 15:12:28 +0800

    fix deployment

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {
   getAuth, createUserWithEmailAndPassword,
-  signInWithEmailAndPassword, signInWithPopup,
+  signInWithEmailAndPassword,
   setPersistence, browserLocalPersistence,
   onAuthStateChanged, signOut, sendPasswordResetEmail,
   signInWithRedirect,
